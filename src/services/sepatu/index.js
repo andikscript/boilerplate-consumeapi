4d5046b4a2ae3d0d00051a4d6fb6bf5896bd8a5b
andikscript <andikscript@gmail.com>
2022-02-21 17:24:28 +0700

    change path

--- src/services/sepatu/index.js
+++ src/services/sepatu/index.js
@@ -1,19 +1,20 @@
+import { BaseURL } from "../../config/BaseUrl";
 import { BaseService } from "../BaseService";
 
 function getSepatu() {
-  return BaseService.get('/produk');
+  return BaseService.get(BaseURL.PRODUK);
 }
 
 function postSepatu(body) {
-  return BaseService.post('/produk', body);
+  return BaseService.post(BaseURL.PRODUK, body);
 }
 
 function updateSepatu(id, body) {
-  return BaseService.put(`/produk/${id}`, body);
+  return BaseService.put(BaseURL.PRODUK_ID(id), body);
 }
 
 function deleteSepatu(id) {
-  return BaseService.delete(`/produk/${id}`);
+  return BaseService.delete(BaseURL.PRODUK_ID(id));
 }
 
 export {
